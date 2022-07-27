d3da9f95d27fad7c15f3d65203a9960fa28db7df
NS <coolsaki.ws@gmail.com>
2022-07-27 11:32:38 +0900

    modify structure of project
    modify layout of project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
 import "./App.css";
-import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import { ThemeProvider, useTheme } from "@material-ui/core/styles";
 import Layout from "layout/Layout";
 
 function App() {
-  const theme = createTheme({
-    breakpoints: {
-      values: {
-        xs: 0,
-        sm: 540,
-        md: 1025,
-        lg: 1367,
-        xl: 1600,
-      },
-    },
-  })
+  const theme = useTheme();
 
   return (
     <ThemeProvider theme={theme}>
---
+++ src/layout/Layout.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useStyles } from "./LayoutStyle";
+import { Login } from "pages/Login/Login";
+import { Register } from "pages/Register/Register";
+
+export default function Layout() {
+  const classes = useStyles();
+
+  return (
+    <Router>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </Router>
+  );
+}
